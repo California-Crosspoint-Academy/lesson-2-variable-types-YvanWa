c79c76854caef68d4baf7fc3161b2ebf700de964
YvanWa <yvanwang26@crosspointacademy.org>
2025-08-22 18:31:44 +0000

    Message

--- Variables.java
+++ Variables.java
@@ -1,5 +1,8 @@
 public class Variables{
     public static void main(String[]args){
-        int x
+        double age = 75.875;
+        String Nation = "PRC";
+        int birthday = 1949;
+        System.out.println(Nation+" was born in "+ birthday + " and right now is about "+age+" years old");
     } 
 }
